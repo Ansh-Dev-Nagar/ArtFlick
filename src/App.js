284cf75288760ee28644b0aa91ed7fce501070b0
Ansh Dev Nagar <anshdevnagar@gmail.com>
2025-03-11 01:16:32 +0530

    Initial commit: Clean setup for ArtFlick project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './img/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src={logo} alt="ArtFlick Logo" className="logo" />
       </header>
+      <main>
+        <h1>Welcome to ArtFlick</h1>
+        <p>Your Art Discovery Platform</p>
+      </main>
     </div>
   );
 }
